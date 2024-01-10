9a7cf03fe7e0faddd41499280192c73f42e3622d
Sharlotte <aaa9810321@gmail.com>
2024-01-10 17:15:22 +0900

    feat: setup multiple tsconfig

--- scripts/watcher.js
+++ scripts/watcher.js
@@ -1,5 +1,6 @@
-var childProcess = require("child_process");
+// @ts-check
 
+const childProcess = require("child_process");
 childProcess.exec("mlogjs script.ts out.mlog", function (error, stdout, stderr) {
   console.log(stdout);
   console.log(stderr);
